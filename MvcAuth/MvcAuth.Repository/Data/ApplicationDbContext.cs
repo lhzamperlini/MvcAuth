e9aac3ceb5c2c0e1ae71cf0c50212493a2b0e790
Luis Henrique Zamperlini de Campos <lhzamperlini@outlook.com>
2023-10-29 11:47:26 -0300

    Adicionando estrutura base do projeto

---
+++ MvcAuth/MvcAuth.Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcAuth.Repository.Data;
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(x => x.GetProperties()
+                               .Where(x => x.ClrType == typeof(string))))
+        {
+            property.SetColumnType("varchar(90)");
+        }
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+          .SelectMany(x => x.GetForeignKeys()))
+        {
+
+            property.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
