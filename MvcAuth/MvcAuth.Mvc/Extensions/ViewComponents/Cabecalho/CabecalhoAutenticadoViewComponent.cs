21ae1f6be8cd03d923eb665c9b5febf7415a6116
Luis Henrique Zamperlini de Campos <lhzamperlini@outlook.com>
2023-11-02 10:21:50 -0300

    Adicionando ViewComponents de cabeçalho

---
+++ MvcAuth/MvcAuth.Mvc/Extensions/ViewComponents/Cabecalho/CabecalhoAutenticadoViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAuth.Mvc.Extensions.ViewComponents.Cabecalho;
+
+[ViewComponent(Name = "CabecalhoAutenticado")]
+public class CabecalhoAutenticadoViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View());
+    }
+}
---
+++ MvcAuth/MvcAuth.Mvc/Extensions/ViewComponents/Cabecalho/CabecalhoPadraoViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAuth.Mvc.Extensions.ViewComponents.Cabecalho;
+
+[ViewComponent(Name = "CabecalhoPadrao")]
+public class CabecalhoPadraoViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View());
+    }
+}
